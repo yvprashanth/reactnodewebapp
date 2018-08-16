bac3537cf99f66386a365d19e7c837d367277817
Prashanth <prashanthyv@gmail.com>
2018-08-15 22:46:18 -0700

    Have a server app now

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   Navbar,
   Jumbotron,
@@ -12,20 +13,36 @@ import {
 } from "react-bootstrap";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      members: []
+    }
+  }
+
+  componentDidMount() {
+    fetch('/api')
+      .then(res => res.json())
+      .then(
+        members => {
+          this.setState({ members: members.message });
+        }
+      );
+  }
+
   render() {
     return <div className="App">
         <Navbar>
           <Navbar.Header>
             <Navbar.Brand>
-              <a href="#home">React-Bootstrap</a>
+              <a href="">
+                Rhino
+              </a>
             </Navbar.Brand>
           </Navbar.Header>
-          <Nav>
+          <Nav pullRight>
             <NavItem eventKey={1} href="#">
-              Link
-            </NavItem>
-            <NavItem eventKey={2} href="#">
-              Link
+              Person Name
             </NavItem>
             <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
               <MenuItem eventKey={3.1}>Action</MenuItem>
@@ -34,26 +51,27 @@ class App extends Component {
               <MenuItem divider />
               <MenuItem eventKey={3.4}>Separated link</MenuItem>
             </NavDropdown>
+            <NavItem eventKey={2} href="#">
+              Logout
+            </NavItem>
           </Nav>
         </Navbar>
-
         <Jumbotron>
-          <h1>Hello, world!</h1>
+          <h1>Rhino!</h1>
           <p>
-            This is a simple hero unit, a simple jumbotron-style component
-            for calling extra attention to featured content or information.
+            Easy to use music teacher software
+            <FontAwesomeIcon icon="coffee" />
           </p>
           <p>
             <Button bsStyle="primary">Learn more</Button>
+            <button type="button" class="btn btn-success">
+              Success
+            </button>
+            <button type="button" class="btn btn-warning">
+              Warning
+            </button>
           </p>
         </Jumbotron>
-
-        <button type="button" class="btn btn-success">
-          Success
-        </button>
-        <button type="button" class="btn btn-warning">
-          Warning
-        </button>
         <p className="App-intro">Prashanth Yerramilli</p>
       </div>;
   }
